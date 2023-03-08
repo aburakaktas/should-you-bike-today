50a74ba4e0117f40eca9686a26ed3d14502ec5bb
Burak Aktas <aburakaktas@gmail.com>
2023-03-08 23:25:58 +0100

    Add new function The new function returns degrees, wind speed and weather type

--- cool.py
+++ cool.py
@@ -9,24 +9,48 @@
 
 import requests
 import json
-
-
 API_KEY = "2b0aa93a8746baccdb80c423dc53b631"
 
-def main():    
+
+class Weather:
+    
+    def __init__(self, degrees, wind_speed, weather_type):
+        self._degrees = degrees
+        self._wind_speed = wind_speed
+        self._weather_type = weather_type
+       
+    # @classmethod 
+    # def get(cls):
+    #     degrees = 
+        
+    def __str__(self):
+        return f"Weather type: {self._weather_type}, Degrees: {self._degrees}, Wind speed: {self._wind_speed}"
     
 
-    print(get_coordinates("Munich"))
+
+
+def main():    
+    coordinates = get_coordinates("tokyo")
+    print(get_weather(coordinates))
+    
 
 def get_coordinates(city_name):
     # should return coordinates from a city name
     headers = {'Accept': 'application/json'}
-    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid=2b0aa93a8746baccdb80c423dc53b631", headers=headers)
-    lat = response.json()[0]["lat"]
-    long = response.json()[0]["lon"]
-    
+    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={API_KEY}", headers=headers)
+    response_json = response.json()
+    lat = response_json[0]["lat"]
+    long = response_json[0]["lon"]
     return {"lat": lat, "long": long}
     
+def get_weather(coordinates):
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates['lat']}&lon={coordinates['long']}&appid={API_KEY}&units=metric")
+    response_json = response.json()
+    degrees = response_json["main"]["temp"]
+    wind_speed = response_json["wind"]["speed"]
+    weather_type = response_json["weather"][0]["main"]
+    weather = Weather(degrees=degrees, wind_speed=wind_speed, weather_type=weather_type)
+    return weather
     
 
 
