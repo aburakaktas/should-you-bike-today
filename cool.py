8c70b85581fc4601037ecfccf7a88204a4f03829
Burak Aktas <aburakaktas@gmail.com>
2023-03-09 22:31:19 +0100

    Refactor with @ozkangokdere: - created a new secret API file - moved the functionality to a Weather class -

--- cool.py
+++ cool.py
@@ -3,57 +3,67 @@
 # get that locations's golden hour time
 # display golden hour expected in XX:XX mins (if less than 24 hours)
 # else display the next possible golden hour expected
-# the logic for golden hour is this: if it is sunny. 
-
+# the logic for golden hour is this: if it is sunny.
 
 
 import requests
-import json
-API_KEY = "2b0aa93a8746baccdb80c423dc53b631"
 
 
 class Weather:
-    
-    def __init__(self, degrees, wind_speed, weather_type):
-        self._degrees = degrees
-        self._wind_speed = wind_speed
-        self._weather_type = weather_type
-       
-    # @classmethod 
-    # def get(cls):
-    #     degrees = 
-        
+    _base_url = "https://api.openweathermap.org/"
+
+    def __init__(self, location):
+        self._location = location
+        self._read_api_key()
+        self._coordinates = self._get_coordinates()
+        self._get_weather()
+
+    # def __init__(self, lat, lon):
+    #     self._coordinates = {"lat": lat, "lon": lon}
+    #     self._read_api_key()
+    #     self._get_weather()
+
+    def _read_api_key(self):
+        with open(".apikey") as file:
+            self._apikey = file.read()
+
+    def _get_coordinates(self):
+        response = requests.get(
+            f"{self._base_url}geo/1.0/direct?q={self._location}&appid={self._apikey}")
+        response_json = response.json()
+        lat = response_json[0]["lat"]
+        lon = response_json[0]["lon"]
+        return {"lat": lat, "lon": lon}
+
+    def _get_weather(self):
+        lat = self._coordinates['lat']
+        lon = self._coordinates['lon']
+        response = requests.get(
+            f"{self._base_url}data/2.5/weather?lat={lat}&lon={lon}&appid={self._apikey}&units=metric")
+        response_json = response.json()
+        self._degrees = response_json["main"]["temp"]
+        self._wind_speed = response_json["wind"]["speed"]
+        self._weather_type = response_json["weather"][0]["main"]
+
     def __str__(self):
-        return f"Weather type: {self._weather_type}, Degrees: {self._degrees}, Wind speed: {self._wind_speed}"
-    
+        return f"Weather type: {self.weather_type}, Degrees: {self.degrees}, Wind speed: {self.wind_speed}"
+
+    @property
+    def degrees(self):
+        return self._degrees
+
+    @property
+    def wind_speed(self):
+        return self._wind_speed
+
+    @property
+    def weather_type(self):
+        return self._weather_type
 
 
-
-def main():    
-    coordinates = get_coordinates("tokyo")
-    print(get_weather(coordinates))
-    
-
-def get_coordinates(city_name):
-    # should return coordinates from a city name
-    headers = {'Accept': 'application/json'}
-    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={API_KEY}", headers=headers)
-    response_json = response.json()
-    lat = response_json[0]["lat"]
-    long = response_json[0]["lon"]
-    return {"lat": lat, "long": long}
-    
-def get_weather(coordinates):
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates['lat']}&lon={coordinates['long']}&appid={API_KEY}&units=metric")
-    response_json = response.json()
-    degrees = response_json["main"]["temp"]
-    wind_speed = response_json["wind"]["speed"]
-    weather_type = response_json["weather"][0]["main"]
-    weather = Weather(degrees=degrees, wind_speed=wind_speed, weather_type=weather_type)
-    return weather
-    
-
-
+def main():
+    weather = Weather("Munich")
+    print(weather)
 
 
 if __name__ == "__main__":
