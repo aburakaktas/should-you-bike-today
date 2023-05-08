fa515075a9ebe2695d2ad5d21af68bc329426df4
Burak Aktas <aburakaktas@gmail.com>
2023-05-08 23:19:09 +0200

    did some refactoring, it works, but at what cost? I think now the code looks very unreadable

--- cool.py
+++ cool.py
@@ -12,6 +12,31 @@ class Result:
     _main_result = None
     _wind_warning = None
     
+    def __init__(self, weather):
+        self.weather = weather
+        self._decide()
+    
+    
+    def _decide(self):
+        if self.weather.degrees >= 20:
+            self.main_result = True
+        elif self.weather.degrees > 15 and self.weather.degrees < 20:
+            if self.weather.weather_type == "Rain":
+                self.main_result = False
+            else:
+                self.main_result = True
+        elif self.weather.degrees > 0:
+            if self.weather.weather_type != "Clear":
+                self.main_result = False
+            else:
+                self.main_result = True
+        if self.weather.wind_speed > 10:
+            self.wind_warning = True
+        else:
+            self.wind_warning = False
+        
+        print(f"Is it cycling weather in {self.weather.location}?\n- {'Yes!' if self.main_result else 'No.'} It is {self.weather.degrees} degrees and weather type is: {self.weather.weather_type}.\nShould i care about wind?\n- {'Yes.' if self.wind_warning else 'No!'} Wind speed is {self.weather.wind_speed} km/h.")
+        
     
     @property
     def main_result(self):
@@ -28,41 +53,32 @@ class Result:
     @wind_warning.setter
     def wind_warning(self, value):
         self._wind_warning = value
-    
-    def __str__(self):
-        return f"Is it cycling weather: {'Yes' if self.main_result else 'No'},\nShould i care about wind: {'Yes' if self.wind_warning else 'No'}"
-        
-    
         
 
 def main():
-    weather = Weather("munich")
-    result = Result()
-    print(weather.get_full_json())
-    # print(f"Weather degrees:{weather.degrees} type:{type(weather.degrees)}")
-    # print(f"Weather type:{weather.weather_type} type:{type(weather.weather_type)}")
-    # print(f"Wind speed:{weather.wind_speed} type:{type(weather.wind_speed)}")
+    weather = Weather("scotland")
+    result = Result(weather)
+    # print(weather.get_full_json())
 
-
-    if weather.degrees >= 20:
-        result.main_result= True
-    elif weather.degrees > 10 and weather.degrees < 20:
-        if weather.weather_type == "Rain":
-            result.main_result = False
-        else:
-            result.main_result = True
-    elif weather.degrees > 0:
-        if weather.weather_type != "Clear":
-            result.main_result = False
-        else:
-            result.main_result = True
+    # if weather.degrees >= 20:
+    #     result.main_result= True
+    # elif weather.degrees > 15 and weather.degrees < 20:
+    #     if weather.weather_type == "Rain":
+    #         result.main_result = False
+    #     else:
+    #         result.main_result = True
+    # elif weather.degrees > 0:
+    #     if weather.weather_type != "Clear":
+    #         result.main_result = False
+    #     else:
+    #         result.main_result = True
     
-    if weather.wind_speed > 10:
-        result.wind_warning = True
-    else:
-        result.wind_warning = False
+    # if weather.wind_speed > 10:
+    #     result.wind_warning = True
+    # else:
+    #     result.wind_warning = False
         
-    print(f"Is it cycling weather in {weather.location}?\n- {'Yes!' if result.main_result else 'No.'} It is {weather.degrees} degrees and weather type is: {weather.weather_type}.\nShould i care about wind?\n- {'Yes.' if result.wind_warning else 'No!'} Wind speed is {weather.wind_speed} km/h.")
+    # print(f"Is it cycling weather in {weather.location}?\n- {'Yes!' if result.main_result else 'No.'} It is {weather.degrees} degrees and weather type is: {weather.weather_type}.\nShould i care about wind?\n- {'Yes.' if result.wind_warning else 'No!'} Wind speed is {weather.wind_speed} km/h.")
         
 
 if __name__ == "__main__":
