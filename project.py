dbcec3682aadbb2923058836ff307ff2e9f3e964
Burak Aktas <aburakaktas@gmail.com>
2023-05-11 23:01:44 +0200

    working on the final function - golden hour

--- project.py
+++ project.py
@@ -7,6 +7,11 @@
 
 
 from weather import Weather
+import datetime
+from astral.sun import sun
+from astral import LocationInfo
+
+
 
 
 class Result:
@@ -52,10 +57,32 @@ def decide_result(degrees, weather_type, wind_speed, location, result: Result):
     return f"Is it cycling weather in {location}?\n- {'Yes!' if result.main_result else 'No.'} It is {degrees} degrees and weather type is: {weather_type}.\nShould I care about wind?\n- {'Yes.' if result.wind_warning else 'No!'} Wind speed is {wind_speed} km/h."
         
 
+def get_location():
+    user_input = input("Where are you?: ")
+    return user_input
+
+
 def main():
-    weather = Weather("munich")
-    result = Result()
-    print(decide_result(weather.degrees, weather.weather_type, weather.wind_speed, weather.location, result))
+    # weather = Weather(get_location())
+    # weather = Weather("munich")
+    # result = Result()
+    # print(decide_result(weather.degrees, weather.weather_type, weather.wind_speed, weather.location, result))
+    city = LocationInfo(name="sad", latitude="34", longitude="43")
+    print((
+        f"Information for {city.name}/{city.region}\n"
+        f"Timezone: {city.timezone}\n"
+        f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
+    ))
+    
+    s = sun(city.observer, date=datetime.date(2009, 4, 22), tzinfo=)
+    print((
+        f'Dawn:    {s["dawn"]}\n'
+        f'Sunrise: {s["sunrise"]}\n'
+        f'Noon:    {s["noon"]}\n'
+        f'Sunset:  {s["sunset"]}\n'
+        f'Dusk:    {s["dusk"]}\n'
+    ))
+    
     
 
 if __name__ == "__main__":
